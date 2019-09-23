f8be72578c9e306017ffda6046657e76e265ba40
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:47:54 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan1/IF210118063Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan1;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Biodata"); 
+         System.out.println("-------------------"); 
+         System.out.println("Nama\t\t: Naufal Asha W."); 
+         System.out.println("Alamat\t\t: Bandung Indah Raya"); 
+         System.out.println("Tempat Lahir\t: Bandung");
+         System.out.println("Tanggal Lahir\t: 8 September 1998"); 
+         System.out.println("-------------------"); 
+         System.out.println("Hobi\t\t:Model Kit"); 
+         System.out.println("MAKES\t\t:Udon"); 
+         System.out.println("MIKES\t\t:Cendol");
+    }
+    
+}
